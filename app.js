b83f4d2dec124bd22fc7c48764fdda5deee8b784
ashik-ahmed <ashikahmed121@gmail.com>
2024-01-17 21:42:45 +0600

    inventory route created

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ app.get('/', (req, res) => {
 
 const userRoute = require('./routes/user.route');
 const categoryRoute = require('./routes/category.route');
+const inventoryRoute = require('./routes/inventory.route');
 
 
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/category', categoryRoute);
+app.use('/api/v1/inventory', inventoryRoute);
 
 
 module.exports = app;
