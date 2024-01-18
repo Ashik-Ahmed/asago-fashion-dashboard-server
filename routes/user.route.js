a233c6e40e2755baa60a4356fbab61e85ab1d838
ashik-ahmed <ashikahmed121@gmail.com>
2024-01-18 11:52:55 +0600

    user controller file created

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const userController = require("../controllers/user.controller.js");
 
 const router = express.Router();
 
 router.route("/")
+    .post(userController.createUser)
     .get((req, res) => {
         res.json("hello user api endpoint")
     })
